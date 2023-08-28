a8ced99e36e9b457cc6e1441ceaa128d19b34bec
colepalm <palm.cole@gmail.com>
2023-08-27 18:57:48 -0600

    feel like this is going backwards but im gonna commit it anyway

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,49 @@
-FROM --platform=linux/amd64 node
-RUN apt update
-RUN apt -y install build-essential
-RUN apt -y install wget
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-RUN apt-get update && apt-get -y install google-chrome-stable
+# Use alpine-based NodeJS base image
+FROM node:16.10
 
+USER root
 
+RUN apt-get update -y
+# We need wget to set up the PPA and xvfb to have a virtual screen and unzip to install the Chromedriver
+RUN apt-get install -y wget xvfb unzip
+# Set up the Chrome PPA
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+# Update the package list and install chrome
+RUN apt-get update -y
+RUN apt-get install -y google-chrome-stable
+# Set up Chromedriver Environment variables
+ENV CHROMEDRIVER_VERSION 114.0.5735.90
+ENV CHROMEDRIVER_DIR /chromedriver
+RUN mkdir $CHROMEDRIVER_DIR
+# Download and install Chromedriver
+RUN wget -q --continue -P $CHROMEDRIVER_DIR "http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+RUN unzip $CHROMEDRIVER_DIR/chromedriver* -d $CHROMEDRIVER_DIR
+# Put Chromedriver into the PATH
+ENV PATH $CHROMEDRIVER_DIR:$PATH
 
-# COPY --from=CHORMEBUILDER /usr/bin/google-chrome-stable /usr/bin/google-chrome-stable
-# Create app directory
-COPY $PWD /crm-connector
-WORKDIR /crm-connector
+RUN apt-get -y install default-jre
 
-# RUN npm install --location=global npm@8.12.1
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
+RUN #apt-get install chromium-browser
+
+RUN apt-get -f install
+
+# Set up timezone for google-chrom-stable question
+ENV TZ=US
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container
 COPY package*.json ./
-RUN node -v
-RUN npm uninstall chromedriver
-RUN npm install
-RUN npm install chromedriver --chromedriver-force-download --detect_chromedriver_version
-# If you are building your code for production
-#RUN npm ci --only=production
 
-# Bundle app source
+# Install project dependencies
+RUN npm install
+
+# Copy the rest of the application code to the container
 COPY . .
 
-EXPOSE 8081
+# Command to run your Selenium tests
 RUN npm run test
+
--- src/test/test.js
+++ src/test/test.js
@@ -1,79 +1,72 @@
 require('chromedriver');
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-const { By, until } = webdriver;
 const fetch = require('node-fetch')
+const { By, until } = webdriver;
 
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const timeout = 99999;
 
-chai.use(chaiAsPromised);
+try {
+  const options = new chrome.Options();
+  options.addArguments(
+      'headless',
+      'disable-gpu',
+  );
+  const driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
 
-const screen = {
-  width: 1040,
-  height: 680
-};
+  describe('Make Wook Post', () => {
+    // before(async () => {
+    //   try {
+    //     await driver.manage().setTimeouts({
+    //       implicit: timeout,
+    //       pageLoad: timeout,
+    //       script: timeout
+    //     })
+    //   } catch(err) {
+    //     console.log(err)
+    //   }
+    // });
+    //
+    // it('generate post', async () => {
+    //   try {
+    //     this.wookPost = await fetch('https://wookmark.fly.dev/generate')
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
+    // })
 
-const timeout = 99999999999999;
+    // it('Direct to main phish page', async () => {
+    //   await driver.get('https://phantasytour.com/bands/phish/threads');
+    //   await driver.wait(until.elementLocated(By.className('hide_overflow')));
+    // });
 
-const options = new chrome.Options()
-
-options.addArguments('--disable-dev-shm-usage')
-options.addArguments('--no-sandbox')
-options.addArguments('--headless')
-
-const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build();
-
-describe('Make Wook Post', () => {
-  before(async () => {
-    try {
-      await driver.manage().setTimeouts({
-        implicit: timeout,
-        pageLoad: timeout,
-        script: timeout
-      })
-    } catch(err) {
-        console.log(err)
-      }
+    // it('Login', async () => {
+    //   await driver.findElement(By.xpath("//*[contains(text(), 'Log in')]")).click();
+    //   await driver.wait(until.elementLocated(By.id('login_username')));
+    //   await driver.findElement(By.id('login_username')).sendKeys('wook_mark');
+    //   await driver.findElement(By.id('login_password')).sendKeys('4iZ!83vZ');
+    //   await driver.findElement(By.className('btn-secondary')).click();
+    //   await driver.wait(until.elementLocated(By.className('hide_overflow')));
+    // });
+    //
+    // it('Find thread', async () => {
+    //   const elements = await driver.findElements(By.className('hide_overflow'));
+    //   await elements[10].click();
+    //   await driver.wait(until.elementLocated(By.className('post_body_container')));
+    // });
+    //
+    // it('Enter wook post', async () => {
+    //   await driver.findElement(By.className('form-control')).sendKeys(this.wookPost);
+    // });
+    //
+    // it('Submit wook post', async () => {
+    //   await driver.findElement(By.className('btn-primary')).click();
+    // })
   });
+} catch (error) {
+  console.log(error)
+}
 
-  it('generate post', async () => {
-    try {
-      const res = await fetch('https://wookmark.fly.dev/generate');
-      this.wookPost = await res.text()
-    } catch (err) {
-      console.log(err)
-    }
-  })
-
-  it('Direct to main phish page', async () => {
-    await driver.get('https://phantasytour.com/bands/phish/threads');
-    await driver.wait(until.elementLocated(By.className('hide_overflow')));
-  });
-
-  it('Login', async () => {
-    await driver.findElement(By.xpath("//*[contains(text(), 'Log in')]")).click();
-    await driver.wait(until.elementLocated(By.id('login_username')));
-    await driver.findElement(By.id('login_username')).sendKeys('wook_mark');
-    await driver.findElement(By.id('login_password')).sendKeys('4iZ!83vZ');
-    await driver.findElement(By.className('btn-secondary')).click();
-    await driver.wait(until.elementLocated(By.className('hide_overflow')));
-  });
-
-  it('Find thread', async () => {
-    const elements = await driver.findElements(By.className('hide_overflow'));
-    await elements[10].click();
-    await driver.wait(until.elementLocated(By.className('post_body_container')));
-  });
-
-  it('Enter wook post', async () => {
-    await driver.findElement(By.className('form-control')).sendKeys(this.wookPost);
-  });
-
-  it('Submit wook post', async () => {
-    await driver.findElement(By.className('btn-primary')).click();
-  })
-});
