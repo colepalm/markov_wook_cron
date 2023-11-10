117bedbe65b243dde084d7d5014ade699ce99200
colepalm <palm.cole@gmail.com>
2023-11-09 22:28:05 -0700

    magic argument

--- src/test/test.py
+++ src/test/test.py
@@ -13,6 +13,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox=600000000')
 chrome_options.add_argument(f"user-agent={user_agent}")
+chrome_options.add_argument('--disable-dev-shm-usage')  
 
 width = 1920
 height = 1080
